d61bbc7e57949b5d258b08341079d52fdb547092
Infy <l22920156@voaxaca.tecnm.mx>
2025-09-11 10:14:39 -0600

    ramaInfy

---
+++ js/DiegoL.js
@@ -0,0 +1,6 @@
+//esto es para ver si funciona esto por parte del alumno diego lopez
+document.getElementById("btnSaludo").addEventListener(
+    "click", function () {
+        alert("Saludos de parte de infy");
+    }
+);
