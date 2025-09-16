574c496d985784663458b2de9f6cec12445f2770
Roman <l23920255@voaxaca.tecnm.mx>
2025-09-16 13:52:26 -0600

    primer commit del proyecto pryinfo2025

---
+++ js/roman.js
@@ -0,0 +1,15 @@
+/*
+Identificate: Soy Roman
+25 años
+*/
+
+
+// Solicita el nombre del usuario
+let nombre = prompt("¿Cuál es tu nombre?");
+
+// Muestra un saludo personalizado
+if (nombre) {
+    alert("¡Hola, " + nombre + "! Bienvenido a tu proyecto.");
+} else {
+    alert("No ingresaste ningún nombre.");
+}
