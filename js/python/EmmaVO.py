1e51640f7e2a7a85a0567c1e3ec45281eb8896ca
Luna <erahinpachecoluna@gmail.com>
2025-09-17 09:21:57 -0600

    calificando práctica

--- js/python/EmmaVO.py
+++ js/python/EmmaVO.py
@@ -3,3 +3,6 @@
 print("Hola, soy Emmanuel y mi banda favorita es System of a Down")
 
 print("organicé mejor mi archivo y ahora le agrego más código")
+
+
+// tu archivo no debe ir dentro de js, saludos!!
